0a714549892f9ee53a20ee1b2bacce3187fe3f82
McKinley <mckinleyslade@yahoo.com>
2023-03-26 08:53:50 -0400

    Updated code to include image display

--- seasonals_dash_st.py
+++ seasonals_dash_st.py
@@ -3,21 +3,11 @@ import os
 
 st.set_page_config(layout="wide")
 
-# Get the directory where the Streamlit app script is located
-script_dir = os.path.dirname(os.path.realpath(__file__))
+file_loc = "."
+charts = [x for x in os.listdir(file_loc) if x.lower().endswith((".jpg", ".jpeg", ".png"))]
 
-# Specify the subdirectory containing the images relative to the script directory
-image_dir = "Dash_images"
-
-# Get the full path to the image directory
-image_dir_path = os.path.join(script_dir)
-
-# Get a list of all image files in the directory
-image_files = os.listdir(image_dir_path)
-
-# Loop over the image files and display them
-for image_file in image_files:
-    image_path = os.path.join(image_dir_path, image_file)
+for x in charts:
+    image_path = os.path.join(file_loc, x)
     st.image(image_path, output_format='PNG')
 
 # for x in charts:
