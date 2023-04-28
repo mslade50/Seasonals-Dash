76e352d578a6a045dbc2f5d6121beb6238d8fa5b
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-04-28 15:49:42 -0400

    Update seasonals_dash_st.py

--- seasonals_dash_st.py
+++ seasonals_dash_st.py
@@ -11,14 +11,14 @@ import plotly.graph_objs as go
 
 def seasonals_chart(tick):
 	ticker=tick
-	cycle_start=1952
+	cycle_start=1951
 	cycle_label='Midterms'
 	cycle_var='pre_election'
 	adjust=0
 	plot_ytd="Yes"
 	all_=""
-	end_date=dt.datetime(2015,12,30)
-	this_yr_end=dt.datetime(2016,11,3)
+	end_date=dt.datetime(2014,12,30)
+	this_yr_end=dt.datetime(2015,12,30)
 
 
 	spx1=yf.Ticker(ticker)
