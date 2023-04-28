5fe54175a90067c0fb641b9e535fa74a819ee1f7
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-04-28 15:00:23 -0400

    Update seasonals_dash_st.py

--- seasonals_dash_st.py
+++ seasonals_dash_st.py
@@ -18,7 +18,7 @@ def seasonals_chart(tick):
 	plot_ytd="Yes"
 	all_=""
 	end_date=dt.datetime(2022,12,30)
-	this_yr_end=dt.datetime(2023,3,23)
+	this_yr_end=dt.datetime(2023,3,13)
 
 	spx1=yf.Ticker(ticker)
 	spx = spx1.history(period="max",end=end_date)
