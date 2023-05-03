27fc8eee557031d35addc89a4a1ddd3c5037a3b0
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-05-03 13:14:22 -0400

    Update seasonals_dash_st.py

--- seasonals_dash_st.py
+++ seasonals_dash_st.py
@@ -544,7 +544,7 @@ def seasonals_chart(tick):
 	)
 	st.plotly_chart(fig)
 
-positions=['^GSPC','SPY','MLCO','cl=f']
+positions=['^GSPC','SPY','MLCO','cl=f','EURUSD=X']
 positions.sort()
 for stock in positions:
 	seasonals_chart(stock)
