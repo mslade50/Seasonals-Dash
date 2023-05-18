c83ea5914cd9451529c53beb5f2e9571061ff59e
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-05-17 20:12:54 -0400

    Update seasonals_dash_st.py

--- seasonals_dash_st.py
+++ seasonals_dash_st.py
@@ -11,7 +11,7 @@ import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 from ta.momentum import RSIIndicator
 
-positions=['jks','tlt','ZW=F']
+positions=['EURCHF=X']
 def seasonals_chart(tick):
 	ticker=tick
 	cycle_start=1951
